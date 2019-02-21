0e59e80ff79a1e269ef3e2daa84f98cfd5490cd7
Josué Basurto <jbasurto@jelp.io>
2019-02-20 22:26:38 -0800

    Changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var ip = require('ip')
 
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log("Server running on port 3000");
 });
 
