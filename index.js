aa6154db45abffe3860f364925fe28483fee7423
Josué Basurto <jbasurto@jelp.io>
2019-02-20 22:08:53 -0800

    Commit inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var ip = require('ip')
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+app.get("/", (req, res, next) => {
+    response = {'response':'Yeah','ip':ip.address()};
+    res.json(response);
+});
